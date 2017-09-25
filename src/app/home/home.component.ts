188efbd3090c019e13d8abb84472c92c871cb636
Jared Carrano <jcarrano@newvisions.org>
2017-09-25 11:21:13 -0400

    adds ngOnInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Product, ProductService } from '../shared/services';
 
 @Component({
   selector: 'ngs-home',
-  styleUrls: [ './home.component.scss' ],
+  styleUrls: ['./home.component.scss'],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   products: Observable<Product[]>;
   constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
     this.products = this.productService.getAll();
   }
 }
