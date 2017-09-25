c844b81a39443e971d016790480c1f05f8e20818
Jared Carrano <jcarrano@newvisions.org>
2017-09-25 14:00:35 -0400

    Making category tabs bookmarkable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild
+} from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Product, ProductService } from '../shared/services';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MdTabGroup } from '@angular/material';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'ngs-home',
   styleUrls: ['./home.component.scss'],
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MdTabGroup) mdTabGroup: MdTabGroup;
 
   products: Observable<Product[]>;
 
@@ -18,16 +28,29 @@ export class HomeComponent implements OnInit {
     'sport'
   ];
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
-    this.products = this.productService.getAll();
+    this.products = this.route.params
+      .switchMap(({ category }) => {   // Note that we use destructuring here
+        return category === 'all' ?
+          this.productService.getAll() :
+          this.productService.getCategory(category);
+      });
+  }
+
+  ngAfterViewInit() {
+    const category = this.route.snapshot.params['category'];
+    this.mdTabGroup.selectedIndex = this.categories.indexOf(category);
   }
 
   onTabChange(tabIndex: number) {
     const category = this.categories[tabIndex];
-    console.log(`Selected category: ${category}`);
-    this.products = this.productService.getCategory(category);
+    this.router.navigate([category], { relativeTo: this.route.parent });
   }
 }
